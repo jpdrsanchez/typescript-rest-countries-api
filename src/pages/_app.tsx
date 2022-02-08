c3424acd8a61fabe29ec3f0d154330e23040092f
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-02-08 12:36:15 -0300

    feat: add nprogress to improve loading ux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,19 @@ import '@fontsource/nunito-sans/300.css'
 import '@fontsource/nunito-sans/400.css'
 import '@fontsource/nunito-sans/600.css'
 import '@fontsource/nunito-sans/800.css'
+import NProgress from 'nprogress'
+import { useRouter } from 'next/router'
 
 import GlobalStyles from 'styles/global'
 import Header from 'components/Header'
 import Switch from 'components/Switch'
 import { useEffect, useState } from 'react'
 import { getStorageItem, setStorageItem } from 'services/localStorage'
+import '../../public/nprogress.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [colorMode, setColorMode] = useState(getStorageItem('colorMode'))
+  const router = useRouter()
 
   const toggleTheme = (colorMode: string) => {
     if (colorMode === 'light') {
@@ -27,6 +31,25 @@ function MyApp({ Component, pageProps }: AppProps) {
   const onThemeChange = () => {
     setColorMode(colorMode === 'light' ? 'dark' : 'light')
   }
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
 
   useEffect(() => {
     toggleTheme(colorMode)
